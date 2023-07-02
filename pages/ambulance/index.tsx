00a71c53efa8b90fbe152e4be048f3f7c6672227
silverag2002 <silverag2002@gmail.com>
2023-07-02 09:36:00 +0530

    Added soem styling to ambulance section

--- pages/ambulance/index.tsx
+++ pages/ambulance/index.tsx
@@ -2,9 +2,28 @@ import MapComponent from "../../app/components/map/MapComponent";
 
 export default function Ambulance() {
   return (
-    <div>
-      <h1>Map Page</h1>
-      <MapComponent />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        // alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1 style={{}}>
+        <span style={{ color: "red" }}>Emergency</span> Services For Ambulance
+      </h1>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ flex: 1 }}>
+          <div>
+            <label>Enter your location</label>
+            <input type="text" />
+          </div>
+        </div>
+        <div style={{ flex: 1 }}>
+          <MapComponent />
+        </div>
+      </div>
     </div>
   );
 }
